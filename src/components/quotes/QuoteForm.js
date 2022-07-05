efb247b44ead9a3c892513d6bf324d5b863dae6b
ZenitsuV <vigneshari19@gmail.com>
2022-07-05 20:38:18 +0530

    2 files modified, 5 added

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useRef, useState } from 'react';
-import { Prompt } from 'react-router-dom';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
